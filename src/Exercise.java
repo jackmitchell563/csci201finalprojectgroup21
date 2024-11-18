721052e3442ec95e3a5879b6d587328ecc75290f
Michelle Lee <mlee9568@usc.edu>
2024-11-17 16:08:08 -0800

    exercise and fitness tracker

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,23 +1,101 @@
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Exercise {
     private int id;
     private int userId;
-    private Date date;
-    private String name;
+    private LocalDate date;
+    private String type; // Changed 'name' to 'type'
+    private double weight;
     private int repetitions;
     private int sets;
-    private int durationMins;
-    
-	public Exercise(int id, int userId, Date date, String name, int repetitions, int sets, int durationMins) {
-		this.id = id;
-		this.userId = userId;
-		this.date = date;
-		this.name = name;
-		this.repetitions = repetitions;
-		this.sets = sets;
-		this.durationMins = durationMins;
-	}
-    
+    private double durationMins;
 
+    public Exercise(int id, int userId, LocalDate date, String type, double weight, int repetitions, int sets, double durationMins) {
+        this.id = id;
+        this.userId = userId;
+        this.date = date;
+        this.type = type;
+        this.weight = weight;
+        this.repetitions = repetitions;
+        this.sets = sets;
+        this.durationMins = durationMins;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getRepetitions() {
+        return repetitions;
+    }
+
+    public void setRepetitions(int repetitions) {
+        this.repetitions = repetitions;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public double getDuration() {
+        return durationMins;
+    }
+
+    public void setDuration(double durationMins) {
+        this.durationMins = durationMins;
+    }
+
+    @Override
+    public String toString() {
+        return "Exercise{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", date=" + date +
+                ", type='" + type + '\'' +
+                ", weight=" + weight +
+                ", repetitions=" + repetitions +
+                ", sets=" + sets +
+                ", durationMins=" + durationMins +
+                '}';
+    }
 }
--- src/FitnessTracker.java
+++ src/FitnessTracker.java
@@ -1,4 +1,5 @@
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FitnessTracker {
@@ -7,11 +8,94 @@ public class FitnessTracker {
 
     public FitnessTracker(int userId) {
         this.userId = userId;
+        this.exercises = new ArrayList<>();
     }
 
     public void addExercise(Exercise exercise) {
         exercises.add(exercise);
     }
 
+    public void removeExercise(int index) {
+        if (index >= 0 && index < exercises.size()) {
+            exercises.remove(index);
+        } else {
+            throw new IndexOutOfBoundsException("Invalid exercise index.");
+        }
+    }
 
+    public List<Exercise> getExercisesByDate(LocalDate date) {
+        List<Exercise> result = new ArrayList<>();
+        for (Exercise exercise : exercises) {
+            if (exercise.getDate().equals(date)) {
+                result.add(exercise);
+            }
+        }
+        return result;
+    }
+
+    public double getTotalWeightLiftedByDate(LocalDate date) {
+        double totalWeight = 0.0;
+        for (Exercise exercise : getExercisesByDate(date)) {
+            totalWeight += exercise.getWeight() * exercise.getRepetitions() * exercise.getSets();
+        }
+        return totalWeight;
+    }
+
+    public double getTotalExerciseTimeByDate(LocalDate date) {
+        double totalTime = 0.0;
+        for (Exercise exercise : getExercisesByDate(date)) {
+            totalTime += exercise.getDuration();
+        }
+        return totalTime;
+    }
+
+    public List<Exercise> getAllExercises() {
+        return new ArrayList<>(exercises);
+    }
+
+    public List<Exercise> getExercisesByDateRange(LocalDate startDate, LocalDate endDate) {
+        List<Exercise> result = new ArrayList<>();
+        for (Exercise exercise : exercises) {
+            if ((exercise.getDate().isEqual(startDate) || exercise.getDate().isAfter(startDate)) &&
+                (exercise.getDate().isEqual(endDate) || exercise.getDate().isBefore(endDate))) {
+                result.add(exercise);
+            }
+        }
+        return result;
+    }
+
+    public double getTotalWeightLifted(LocalDate startDate, LocalDate endDate) {
+        double totalWeight = 0.0;
+        for (Exercise exercise : getExercisesByDateRange(startDate, endDate)) {
+            totalWeight += exercise.getWeight() * exercise.getRepetitions() * exercise.getSets();
+        }
+        return totalWeight;
+    }
+
+    public double getAverageDuration(LocalDate startDate, LocalDate endDate) {
+        List<Exercise> filteredExercises = getExercisesByDateRange(startDate, endDate);
+        if (filteredExercises.isEmpty()) {
+            return 0.0;
+        }
+        double totalDuration = 0.0;
+        for (Exercise exercise : filteredExercises) {
+            totalDuration += exercise.getDuration();
+        }
+        return totalDuration / filteredExercises.size();
+    }
+
+    public void displayHistoricalData(LocalDate startDate, LocalDate endDate) {
+        System.out.println("Historical Data for User ID: " + userId);
+        System.out.println("From: " + startDate + " To: " + endDate);
+        System.out.println("Total Weight Lifted: " + getTotalWeightLifted(startDate, endDate) + " kg");
+        System.out.println("Average Exercise Duration: " + getAverageDuration(startDate, endDate) + " minutes");
+        System.out.println("Details of Exercises:");
+        for (Exercise exercise : getExercisesByDateRange(startDate, endDate)) {
+            System.out.println(exercise);
+        }
+    }
+
+    public void clearAllExercises() {
+        exercises.clear();
+    }
 }
